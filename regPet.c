57d1c640e13e6356c94ea2beb0262e37b42b58a5
JpSCabral <112001641+JpSCabral@users.noreply.github.com>
2022-11-11 09:20:53 -0300

    Update regPet.c

--- regPet.c
+++ regPet.c
@@ -1,27 +1,31 @@
-int main (void){
-
+#include<stdio.h>
+#include<stdlib.h>
+main (void){
+int idade, cpf, quant;
+char nome, genero, raca;
+    
     printf("Digite quantos Pets serao cadastrados:\n");
- scanf("%d", & );
+ scanf("%d", &quant);
 
    for (i=0; i < quant; i++){
     printf("Digite o nome do Pet:\n");
-    scanf("%s", & );
+    scanf("%s", &nome);
     fflush (stdin);
 
     printf("Digite a idade do Pet:\n");
-    scanf("%s", & );
+    scanf("%s", &idade );
     fflush (stdin);
 
     printf("Digite o genero do Pet:\n");
-    scanf("%s", & );
+    scanf("%s", &genero );
     fflush (stdin);
     
     printf("Digite a raca do Pet:\n");
-    scanf("%s", & );
+    scanf("%s", &raca );
     fflush (stdin);
 
     printf("Digite o CPF do tutor:\n");
-    scanf("%s", & );
+    scanf("%s", &cpf);
     fflush (stdin);
   }
   return 0;
